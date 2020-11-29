301432197c9b2ac5bd44391ccb27d108e74a60a9
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2020-11-29 13:01:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks('https://anapioficeandfire.com/api/books')
+})
+
+let books=[];
+
 function getPost(dd){
   for (let i=0; i<dd.length; i++){
-    let bookname=[];
-    bookname.push(json[i].name);
+   let book={name };
+   book.name=dd[i].name
+   books.push(book);
   }
-  console.log(bookname);
+  renderBooks(books)
 }
 
-function fetchBooks() {
-  fetch('https://anapioficeandfire.com/api/books')
+function fetchBooks(url) {
+  fetch(url)
   .then(resp => resp.json())
   .then(json => getPost(json));
 }
+fetchBooks('https://anapioficeandfire.com/api/books');
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.innerText = book.name;
     main.appendChild(h2);
   })
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
-

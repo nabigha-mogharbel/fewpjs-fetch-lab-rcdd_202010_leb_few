a4cd688f144e2160fd70112269f06afec76988e7
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2020-11-29 13:18:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-function fetchBooks() {
+let books=[];
 
+//function getPost(dd){
+  //for (let i=0; i<dd.length; i++){
+   //et book={name };
+   //book.name=dd[i].name
+   //books.push(book);
+//  }
+ // renderBooks(books);
+//}
+
+function fetchBooks() {
+   return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
 }
 
+
 function renderBooks(books) {
-  const main = document.querySelector('main')
+  const main = document.querySelector('main');
   books.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = book.name
-    main.appendChild(h2)
-  })
+    const h2 = document.createElement('h2');
+    h2.innerHTML = book.name;
+    main.appendChild(h2);
+  });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
-})
+});
